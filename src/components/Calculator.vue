bfb7eda4c95f57925cc864d4898e8328a8b4642e
Christian Jay <christianquimporeyes@gmail.com>
2022-12-06 19:01:02 +0800

    refactor

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,79 +1,58 @@
 <script setup>
-import { ref, computed, watch, watchEffect, onMounted } from 'vue'
-import dayjs from "dayjs"
-import dayjsBusinessDays from "dayjs-business-days"
-import { round } from "mathjs/number"
+    const currentSalary = ref()
+    const properSalary = ref()
+    const firstDate = ref()
+    const secondDate = ref()
 
-dayjs.extend(dayjsBusinessDays)
+    // get initial differential amount
+    const initialDifferentialAmount = computed(()=>{
+      return Math.max(0, properSalary.value - currentSalary.value)
+    })
 
-const currentSalary = ref(null)
-const properSalary = ref(null)
-const firstDate = ref("")
-const secondDate = ref("")
-const input1 = ref(null)
 
-onMounted(() => {
-  input1.value.focus()
-})
+    // check the first day and last day of dates given
+    // determine first day of 1st date
+    const firstDayOfFirstDate = computed(()=>{
+      if(dayjs(firstDate.value).isValid()){
+        return dayjs(firstDate.value).startOf("month").format("MM/DD/YYYY")
+      } else {
+        return 0
+      }
+    })
 
-watch(currentSalary, () => {
-  currentSalary.value = currentSalary.value.replace(/\D/g, "")
-    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-})
+    // determine last day of 1st date
+    const lastDayOfFirstDate = computed(() => {
+      if(dayjs(firstDate).isValid()) {
+        return dayjs(firstDate.value).endOf("month").format("MM/DD/YYYY")
+      } else {
+        return 0
+      }
+    })
 
-watch(properSalary, () => {
-  properSalary.value = properSalary.value.replace(/\D/g, "")
-    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-})
 
-watch(firstDate, () => {
-  firstDate.value = firstDate.value.replace(/^\d{2}\/\d{2}\/\d{4}$/)
-})
+        // determine first day of 1st date
+    const firstDayOfSecondDate = computed(()=>{
+      if(dayjs(secondDate.value).isValid()){
+        return dayjs(secondDate.value).startOf("month").format("MM/DD/YYYY")
+      } else {
+        return 0
+      }
+    })
 
-watch(secondDate, () => {
-  secondDate.value = secondDate.value.replace(/\D/g, "")
-    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-})
-
-const initialDifferentialAmount = computed(() => {
-  return (properSalary.value - currentSalary.value <= 0) ? 0 : properSalary.value - currentSalary.value
-})
-
-const firstDayOfFirstDate = computed(() => {
-  return (dayjs(firstDate.value).startOf("month").format("MM/DD/YYYY") === "Invalid Date") ? 0 : dayjs(firstDate.value).startOf("month").format("MM/DD/YYYY")
-})
-
-const lastDayOfFirstDate = computed(() => {
-  return (dayjs(firstDate.value).endOf("month").format("MM/DD/YYYY") === "Invalid Date") ? 0 : dayjs(firstDate.value).endOf("month").format("MM/DD/YYYY")
-})
+    // determine last day of 1st date
+    const lastDayOfSecondDate = computed(() => {
+      if(dayjs(secondDate).isValid()) {
+        return dayjs(secondDate.value).endOf("month").format("MM/DD/YYYY")
+      } else {
+        return 0
+      }
+    })
 
 </script>
 
 
 <template>
-  <div class="mt-6">
-    <input type="text" placeholder="Current Salary" class="w-56 text-2xl bg-gray-300 p-3 rounded-lg focus:outline-none"
-      v-model="currentSalary" ref="input1" maxlength="10">
-  </div>
 
-  <div class="mt-6">
-    <input type="text" placeholder="Proper Salary" class="w-56 text-2xl bg-gray-300 p-3 rounded-lg focus:outline-none"
-      v-model="properSalary" maxlength="10">
-  </div>
-
-  <div class="mt-6">
-    <input type="text" placeholder="MM/DD/YYYY" class="w-56 text-2xl bg-gray-300 p-3 rounded-lg focus:outline-none"
-      v-model="firstDate" maxlength="10">
-  </div>
-
-  <div class="mt-6">
-    <input type="text" placeholder="MM/DD/YYYY" class="w-56 text-2xl bg-gray-300 p-3 rounded-lg focus:outline-none"
-      v-model="secondDate" maxlength="10">
-  </div>
 
 </template>
-    
-<style scoped>
-
-</style>
     
