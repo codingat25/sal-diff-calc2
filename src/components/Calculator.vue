bd292ef4adc1cb87a18dc940c50f9c1156b18d19
Christian Jay <christianquimporeyes@gmail.com>
2022-12-06 23:00:31 +0800

    3days till finish?

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,58 +1,106 @@
 <script setup>
-    const currentSalary = ref()
-    const properSalary = ref()
-    const firstDate = ref()
-    const secondDate = ref()
+import { ref, computed, watch } from "vue";
+import dayjs from "dayjs";
+import dayBusinessDays from "dayjs-business-days";
 
-    // get initial differential amount
-    const initialDifferentialAmount = computed(()=>{
-      return Math.max(0, properSalary.value - currentSalary.value)
-    })
+const currentSalary = ref(0);
+const properSalary = ref(0);
+const firstDate = ref("12/01/2022");
+const secondDate = ref("12/31/2022");
 
+// get initial differential amount
+const initialDifferentialAmount = computed(() => {
+  return Math.max(0, properSalary.value - currentSalary.value);
+});
 
-    // check the first day and last day of dates given
-    // determine first day of 1st date
-    const firstDayOfFirstDate = computed(()=>{
-      if(dayjs(firstDate.value).isValid()){
-        return dayjs(firstDate.value).startOf("month").format("MM/DD/YYYY")
-      } else {
-        return 0
-      }
-    })
+// check the first day and last day of dates given
+// check first date
+const checkFirstDate = computed(() => {
+  return dayjs(firstDate.value).isSame(dayjs(firstDate.value).startOf("month"));
+});
 
-    // determine last day of 1st date
-    const lastDayOfFirstDate = computed(() => {
-      if(dayjs(firstDate).isValid()) {
-        return dayjs(firstDate.value).endOf("month").format("MM/DD/YYYY")
-      } else {
-        return 0
-      }
-    })
+// check second date
+const checkSecondDate = computed(() => {
+  return dayjs(secondDate.value).isSame(
+    dayjs(secondDate.value).endOf("month"),
+    "date"
+  );
+});
 
+//calculate difference in months
+const differenceInMonths = computed(() => {
+  if (dayjs(firstDate.value).isSame(dayjs(secondDate.value), "month")) {
+    return dayjs(secondDate.value).diff(firstDate.value, "month") + 1;
+  } else {
+    return dayjs(secondDate.value).diff(firstDate.value, "month");
+  }
+});
 
-        // determine first day of 1st date
-    const firstDayOfSecondDate = computed(()=>{
-      if(dayjs(secondDate.value).isValid()){
-        return dayjs(secondDate.value).startOf("month").format("MM/DD/YYYY")
-      } else {
-        return 0
-      }
-    })
+//calculate total calendar days
+const totalCalendarDaysFirst = computed(() => {
+  if (
+    dayjs(firstDate.value).isSame(dayjs(firstDate.value).startOf("month")) &&
+    dayjs(lastDayOfFirstDate.value).isSame(
+      dayjs(lastDayOfFirstDate.value).endOf("month")
+    )
+  ) {
+    return dayjs(lastDayOfFirstDate.value).diff(firstDate.value, "day") + 1;
+  } else {
+    return dayjs(lastDayOfFirstDate.value).diff(firstDate.value, "day");
+  }
+});
 
-    // determine last day of 1st date
-    const lastDayOfSecondDate = computed(() => {
-      if(dayjs(secondDate).isValid()) {
-        return dayjs(secondDate.value).endOf("month").format("MM/DD/YYYY")
-      } else {
-        return 0
-      }
-    })
+const totalCalendarDaysSecond = computed(() => {
+  if (
+    dayjs(secondDate.value).isSame(dayjs(secondDate.value).startOf("month")) &&
+    dayjs(lastDayOfsecondDate.value).isSame(
+      dayjs(lastDayOfsecondDate.value).endOf("month")
+    )
+  ) {
+    return dayjs(lastDayOfsecondDate.value).diff(secondDate.value, "day") + 1;
+  } else {
+    return dayjs(lastDayOfsecondDate.value).diff(secondDate.value, "day");
+  }
+});
 
+//calculate total number of days in a month
+
+//1st date
+const fullMonthOfFirstDate = computed(() => {
+  if (
+    dayjs(firstDayOfFirstDate.value).isSame(
+      dayjs(firstDayOfFirstDate.value).startOf("month")
+    ) &&
+    dayjs(lastDayOfFirstDate.value).isSame(
+      dayjs(lastDayOfFirstDate.value).endOf("month")
+    )
+  ) {
+    return (
+      dayjs(lastDayOfFirstDate.value).diff(firstDayOfFirstDate.value, "day") + 1
+    );
+  } else {
+    return 0;
+  }
+});
+
+//2nd date
+const fullMonthOfSecondDate = computed(() => {
+  if (
+    dayjs(firstDayOfSecondDate.value).isSame(
+      dayjs(firstDayOfSecondDate.value).startOf("month")
+    ) &&
+    dayjs(lastDayOfSecondDate.value).isSame(
+      dayjs(lastDayOfSecondDate.value).endOf("month")
+    )
+  ) {
+    return (
+      dayjs(lastDayOfSecondDate.value).diff(firstDayOfSecondDate.value, "day") +
+      1
+    );
+  } else {
+    return 0;
+  }
+});
 </script>
 
-
-<template>
-
-
-</template>
-    
+<template></template>
